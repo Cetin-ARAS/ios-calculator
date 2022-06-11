8ac7725fa5583a0f338ebede9acaf6da8d58aa5d
ÇETİN <cetinnnarasss@gmail.com>
2022-06-11 13:05:38 +0300

    cont.

---
+++ app.js
@@ -0,0 +1,35 @@
+//* =================================================
+//*                     IOS CALCULATOR
+//* =================================================
+const prevDisp = document.querySelector('.previous-display');
+const currDisp = document.querySelector('.current-display');
+
+const btnContainer = document.querySelector('.buttons-container');
+
+let currOperand = '';
+let previousOperand = '';
+
+//? Butonlari tasiyan container icin event tanimlamasi
+btnContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('num')) {
+    appendNumber(e.target.textContent);
+    updateDisplay();
+  }
+  if(e.target)
+});
+
+const appendNumber = (num) => {
+  //? Eger ilk olarak  0 girilmisse geri don
+  if (!currOperand && num === '0') return;
+
+  //? Eğer şu anki sayi . ise ve önceki girilen sayi . iceriyorsa geri don
+  if (num === '.' && currOperand.includes('.')) return;
+  
+  if (currOperand.length > 10) return;
+  //? Girilen sayilari birlestir.
+  currOperand += num;
+};
+
+const updateDisplay = () => {
+  currDisp.textContent = currOperand;
+};
